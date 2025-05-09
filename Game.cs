c0701e0c0b828a1bd61989e77213b9051c2a1b03
ArielVaknin58 <arielvaknin5819@gmail.com>
2025-05-09 16:51:53 +0300

    initial commit
    Created class Game with some members and implemented the string Guess generating fucntion

---
+++ Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bullseye
+{
+    internal class Game
+    {
+        private const int m_StringLength = 4;
+        private char[] m_CorrectGuess { get; set; } = new char[m_StringLength];
+        private int m_MaxGuesses { get; set; } = 10;
+        private int m_GuessCounter { get; set; } = 0;
+
+
+        public char[] GuessString()
+        {
+            Random random = new Random();
+            int[] PreviousGuesses = new int[m_StringLength];
+            for (int i = 0; i < m_StringLength; i++)
+            {
+                int Letter = random.Next(8);
+                while (PreviousGuesses.Contains(Letter))
+                {
+                    Letter = random.Next(8);
+                }
+                PreviousGuesses[i] = Letter;
+                Letter += 65; //To get the ASCII value of a letter between A-J
+                m_CorrectGuess[i] = (char)Letter;
+                
+            }
+
+            return m_CorrectGuess;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Bullseye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(game.GuessString());
+                Console.ReadLine();
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
